4105f67a2778ee598a6a926e82333114d16a8a4a
cyril <cryl91@gmail.com>
2025-03-20 03:09:14 +0530

    First

---
+++ checkinstalled.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+user=$(id -u)
+name=$0
+file=/home/ec2-user/Linux/$name.log
+
+validate(){
+if [ $1 -ne 0 ] 
+then
+        echo "Installing $2. Failure"
+        exit 1
+else
+        echo "Instaled $2. Success"
+fi
+
+}
+
+for i in $@
+do 
+yum list installed $i
+if [ $i -ne 0 ]
+    then 
+        echo "$i not installed,lets install it"
+        yum install $1 -y
+        validate $? $1
+    else
+        echo "$i is already installed"
+    fi
+done
