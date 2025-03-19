8aa3b1ca22becca4760c0233e144cc765aaad17c
cyril <cryl91@gmail.com>
2025-03-20 03:48:53 +0530

    First

--- while.sh
+++ while.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-a=1
-while [ $a -le 5 ] 
-do 
-echo "$a"
-((a++))
-done
+#a=1
+#while [ $a -le 5 ] 
+#do 
+#echo "$a"
+#((a++))
+#done
+
+while read line
+do
+echo "$line"
+done < "code.sh"
