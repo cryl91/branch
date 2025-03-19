9d9a6cfc3db10eee2b8c805ac0810b977a77197c
cyril <cryl91@gmail.com>
2025-03-20 02:59:04 +0530

    First

--- functions.sh
+++ functions.sh
@@ -21,10 +21,20 @@ then
         exit 1
 fi
 
+yum list available git #check if git already installed or not
+if [$? -ne 0]
+then 
 yum install git -y &>> $file
-
 validate $? "Installing git"
+else
+echo "git installed"
+fi
 
+yum list available postfix #check if git already installed or not
+if [$? -ne 0]
+then 
 yum install postfix -y &>> $file
-
-validate $? "Installing Postfix"
+validate $? "Installing postfix"
+else 
+echo "postfix installed"
+fi
