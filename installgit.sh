e5520764413978e631457fb5eac41835255f0d44
cyril <cryl91@gmail.com>
2025-03-20 00:59:08 +0530

    First

---
+++ installgit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+user=$(id -u)
+if [ $user -ne 0 ]
+then
+        echo "not root user"
+        exit 1
+fi
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then
+        echo "failure"
+exit 1
+else
+        echo "success"
+fi
---
+++ othercode.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+date=$(date)
+a=hi
+b=yes
+echo "$a"
+echo "$b"
+echo "$date"
+
+#Addition
+#c=$1
+#d=$2
+#Sum=$((c+d))
+#echo "$Sum"
+
+#Read Command
+#echo "enter user"
+#read user
+
+#echo "enter pass"
+#read -s pass
+
+#echo "User is $user, Pass is $pass"
+
+#Array
+#array=("abc" "cdf" "xyz")
+#echo "${array[@]}"
+
+#IF
+#num=$1
+#if [ $num -gt 2 ]
+#then
+#       echo "$num is greater than 2"
+#else
+#       echo "no"
+
+#fi
