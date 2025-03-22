0a505cd6b0518b155e7ea92877dce69c63689196
cyril <cryl91@gmail.com>
2025-03-22 22:35:41 +0530

    First

---
+++ awsec2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+names=("mongodb" "reddis")
+instancetype=""
+imageid=ami-08b5b3a93ed654d19
+secgrp=sg-048539b811300092e
+
+for i in ${names[@]}
+do
+    if [ $i == "mongodb"]
+    then 
+    instancetype="t3.medium"
+    else
+    instancetype="t2.micro"
+    fi
+echo "creating $i"
+aws ec2 run-instances --image-id $imageid --count 1 --instance-type $instancetype 
+
+done
