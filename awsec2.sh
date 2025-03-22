8da0222a90d0c8091321f02555ea73e92a1b3194
cyril <cryl91@gmail.com>
2025-03-22 22:43:55 +0530

    First

--- awsec2.sh
+++ awsec2.sh
@@ -14,6 +14,6 @@ do
     instancetype="t2.micro"
     fi
 echo "creating $i"
-aws ec2 run-instances --image-id $imageid --count 1 --instance-type $instancetype 
-
+ipaddress=$(aws ec2 run-instances --image-id $imageid --count 1 --instance-type $instancetype | jq -r '.instance[0].PrivateIpAddress')
+echo "$ipaddress"
 done
