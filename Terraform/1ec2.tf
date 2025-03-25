cc7ace79050374d2fd3ec3ea5fa7767afb7b552e
cyril <cryl91@gmail.com>
2025-03-26 04:30:15 +0530

    First

--- Terraform/1ec2.tf
+++ Terraform/1ec2.tf
@@ -2,3 +2,9 @@
 #   ami                     = "ami-08b5b3a93ed654d19"
 #   instance_type           = "t2.micro"
 # }
+
+resource "aws_instance" "myinstance" 
+{
+   ami                     = var.ami_id
+   instance_type           = var.instance_type
+ }
