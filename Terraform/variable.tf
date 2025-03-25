a82ddacafd9bc1b6998986aa6adb490edde1157a
cyril <cryl91@gmail.com>
2025-03-26 04:34:48 +0530

    First

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -1,11 +1,11 @@
 variable "ami_id"{
 type = string
-value = "ami-08b5b3a93ed654d19"
+default = "ami-08b5b3a93ed654d19"
 }
 
 variable "instance_type"{
 type = string
-value = "t2.micro"
+default = "t2.micro"
 }
 
 
