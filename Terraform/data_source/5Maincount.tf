99707f15dd0cf96993a8af8d8f64d8fc7c31d463
cyril <cryl91@gmail.com>
2025-03-27 04:41:57 +0530

    First

--- Terraform/data_source/5Maincount.tf
+++ Terraform/data_source/5Maincount.tf
@@ -1,23 +1,11 @@
 resource "aws_instance" "myinstance" {
-   count = "10"
+   
    ami                     = var.ami_id
-   instance_type           = var.instance_name[count.index] == "mongodb" ? "t2.small" : "t2.micro"
+   instance_type           = "t2.micro"
    
     # tags = {
     #   name = "mongodb"
     #   Envirnoment = "dev"
     # }
 
-    tags = {
-    name = var.instance_name[count.index]
- } 
-}
-
-resource "aws_route53_record" "record" {
-  count = 10
-  zone_id = #give hosted zone id
-  name    = "${var.instance_name[count.index]}.joindevops.online" #interpolation ie fixed value "joindevops.online" is combined with variable
-  type    = "A"
-  ttl     = 1
-  records = [aws_instance.myinstance[count.index].private_ip]
-}
+   } 
