5a9014b899650aad2e249eb1e2dc99055017e39d
cyril <cryl91@gmail.com>
2025-03-28 02:53:24 +0530

    First

--- Terraform/count_index/4count.tf
+++ Terraform/count_index/4count.tf
@@ -13,11 +13,11 @@ resource "aws_instance" "myinstance" {
  } 
 }
 
-resource "aws_route53_record" "record" {
-  count = 10
-  zone_id = #give hosted zone id
-  name    = "${var.instance_name[count.index]}.joindevops.online" #interpolation ie fixed value "joindevops.online" is combined with variable
-  type    = "A"
-  ttl     = 1
-  records = [aws_instance.myinstance[count.index].private_ip]
-}
+# resource "aws_route53_record" "record" {
+#   count = 10
+#   zone_id = #give hosted zone id
+#   name    = "${var.instance_name[count.index]}.joindevops.online" #interpolation ie fixed value "joindevops.online" is combined with variable
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.myinstance[count.index].private_ip]
+# }
