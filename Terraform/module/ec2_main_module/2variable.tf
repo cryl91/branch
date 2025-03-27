75bc3e3f20b65fca497f44bddb525a3c140017ed
cyril <cryl91@gmail.com>
2025-03-28 04:44:23 +0530

    First

---
+++ Terraform/module/ec2_main_module/2variable.tf
@@ -0,0 +1,8 @@
+ 
+variable "ami_id"{
+default = "ami-08b5b3a93ed654d19"
+}
+
+variable "instance_type"{
+default = "t2.micro"
+}
--- Terraform/module/ec2_main_module/3main.tf
+++ Terraform/module/ec2_main_module/3main.tf
@@ -1,3 +1,4 @@
+#In this folder you will apply terraform init,plan,apply
 module "ec2" { 
 source = "../ec2_modules"
 ami_id = "ami-08b5b3a93ed654d19"
