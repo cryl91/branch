1fe52a2da6021c5c05005e4ede2479cd781e3eba
cyril <cryl91@gmail.com>
2025-03-28 01:27:28 +0530

    First

--- Terraform/For_each/4ec2.tf
+++ Terraform/For_each/4ec2.tf
@@ -1,8 +1,3 @@
-# resource "aws_instance" "myinstance" {
-#   ami                     = "ami-08b5b3a93ed654d19"
-#   instance_type           = "t2.micro"
-# }
-
 resource "aws_instance" "myinstance" {
    for_each = var.instances
    ami                     = var.ami_id
@@ -11,3 +6,16 @@ resource "aws_instance" "myinstance" {
       name = each.value
    }
  } 
+
+ output "aws_instance_info" { 
+   value = aws_instance.myinstance
+ }
+
+# resource "aws_route53_record" "record" {
+#   for_each = aws_instance.instances #You can give like this also
+#   zone_id = #give hosted zone id
+#   name    = "${var.instance_name[count.index]}.joindevops.online" #interpolation ie fixed value "joindevops.online" is combined with variable
+#   type    = "A"
+#   ttl     = 1
+#   records = [ each.key == "web" ? each.value.public_ip : each.value.private_ip ]
+# }
