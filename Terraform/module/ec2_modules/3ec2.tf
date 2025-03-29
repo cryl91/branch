7d0ccae35104635f95914d70779b765f396100af
cyril <cryl91@gmail.com>
2025-03-29 20:00:45 +0530

    First

--- Terraform/module/ec2_modules/3ec2.tf
+++ Terraform/module/ec2_modules/3ec2.tf
@@ -2,3 +2,7 @@ resource "aws_instance" "myinstance" {
    ami                     = var.ami_id
    instance_type           = var.instance_type 
  } 
+
+output "instance" {
+  value = aws_instance.myinstance.public_ip 
+}
