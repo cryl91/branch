28b6e38a1a1d492953adab3fb155d1163ae9c8ef
cyril <cryl91@gmail.com>
2025-03-30 18:29:11 +0530

    First

--- Terraform/userdata/ec2.tf
+++ Terraform/userdata/ec2.tf
@@ -4,7 +4,7 @@ module "ec21_instance" {
   name = "ec21"  
   ami = "ami-08b5b3a93ed654d19"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.sg.name]
+  vpc_security_group_ids = [aws_security_group.sg.id]
   subnet_id = aws_subnet.subnetpublic.id
   user_data = file("install.sh")
 
