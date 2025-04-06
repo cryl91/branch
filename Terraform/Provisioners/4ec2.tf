421dce055bda5efc407638fc35c867290c3cc37d
cyril <cryl91@gmail.com>
2025-04-07 03:47:10 +0530

    First

--- Terraform/Provisioners/4ec2.tf
+++ Terraform/Provisioners/4ec2.tf
@@ -1,21 +1,31 @@
-# resource "aws_instance" "myinstance" {
-#   ami                     = "ami-08b5b3a93ed654d19"
-#   instance_type           = "t2.micro"
-# }
-
+#This is Local Exec
 resource "aws_instance" "myinstance" {
    ami                     = var.ami_id
    instance_type           = var.instance_type
    #instance_type = data.aws_ssm_parameter.instance_type.value #to use the parameter store value 
-
-    # tags = {
-    #   name = "mongodb"
-    #   Envirnoment = "dev"
-    # }
-
     tags = var.tags
 
   provisioner "local-exec" {
     command = "echo ${self.private_ip} > file1"
   }
 } 
+
+#This is Remote Exec
+resource "aws_instance" "remote" {
+  ami                     = var.ami_id
+  instance_type           = var.instance_type
+
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = "devops123"
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx",
+      "sudo systemctl start nginx",
+    ]
+  }
+}
