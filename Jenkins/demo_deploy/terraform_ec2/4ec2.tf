599d044eef434561b37a14b1c9c6805aaba67636
cyril <cryl91@gmail.com>
2025-04-09 22:43:42 +0530

    First

--- Jenkins/demo_deploy/terraform_ec2/4ec2.tf
+++ Jenkins/demo_deploy/terraform_ec2/4ec2.tf
@@ -23,60 +23,58 @@ resource "aws_instance" "catalogue" {
     tags = var.tags
  } 
 
-#  resource "null_resource" "cluster" {
-#   //Be used to trigger actions only when certain inputs change
-#   triggers = {
-#   instance_id = aws_instance.catalogue.id
-#    }
+ resource "null_resource" "cluster" {
+   //Be used to trigger actions only when certain inputs change
+   triggers = {
+   instance_id = aws_instance.catalogue.id
+    }
 
-# #Bootstrap script can run on any instance of the cluster
-# #So we just choose the first in this case
-# connection {
-#   type = "ssh"
-#   user = "centos"
-#   password = "devops123"
-#   host= self.private_ip
-# }
+ #Bootstrap script can run on any instance of the cluster
+ #So we just choose the first in this case
+ connection {
+  type        = "ssh"
+  user        = "ec2-user"
+  private_key = file("terraform.pem") # Path to your private key
+  host        = self.private_ip
+}
 
-# //to copy the file into a folder
-# provisioner "file" { 
-#   source ="catalogue.sh"
-#   destination = "/tmp/catalogue.sh"
-# }
 
-# //here write the commands that needs to be executed
-#  provisioner "remote-exec" {
-#    inline = [
-#       "chmod +x /tmp/catalogue.sh",
-#       "sudo su /tmp/catalogue.sh ${var.app_version}"
-#    ]
-#  }
+ //to copy the file into a folder
+ provisioner "file" { 
+   source ="catalogue.sh"
+   destination = "/tmp/catalogue.sh"
+ }
 
-#  }
+ //here write the commands that needs to be executed
+  provisioner "remote-exec" {
+    inline = [
+       "chmod +x /tmp/catalogue.sh",
+       "sudo su /tmp/catalogue.sh"
+    ]
+  }
+
+  }
  
-#  output "app_version" {
-#    value = var.app_version  
-#  }
 
-# //Stop the instance and take the ami_id and delete instance
-#  resource "aws_ec2_instance_state" "catalogue_instance" {
-#   instance_id = aws_instance.catalogue.id
-#   state       = "stopped"
-# }
-#  //taking ami_id
-#   resource "aws_ami_from_instance" "catalogue_ami" {
-#   name = "My_instance's Ami"
-#   source_instance_id = aws_instance.catalogue.id
-# }
+ //Stop the instance and take the ami_id and delete instance
+  resource "aws_ec2_instance_state" "catalogue_instance" {
+   instance_id = aws_instance.catalogue.id
+   state       = "stopped"
+ }
+  //taking ami_id
+   resource "aws_ami_from_instance" "catalogue_ami" {
+   name = "My_instance's Ami"
+   source_instance_id = aws_instance.catalogue.id
+ }
 
-# //delete instance using aws command line. But for this to run you must have aws command line installed on the jenkins server
-# resource "null_resource" "delete instance" {
-#   triggers = {
-#   instance_id = aws_ami_from_instance.catalogue_ami.id
-#    }
+ //delete instance using aws command line. But for this to run you must have aws command line installed on the jenkins server
+ resource "null_resource" "delete instance" {
+   triggers = {
+   instance_id = aws_ami_from_instance.catalogue_ami.id
+    }
 
-# provisioner "local-exec" {
-#     command = "aws ec2 terminate-instances --instance-ids aws_instance.catalogue.id"
-#   }
-# }
-# //The remaining process is alb steps = load balancer,targetgroup,listener,rule,launch template,autoscaling group,auroscaling policy
+ provisioner "local-exec" {
+     command = "aws ec2 terminate-instances --instance-ids aws_instance.catalogue.id"
+   }
+ }
+ //The remaining process is alb steps = load balancer,targetgroup,listener,rule,launch template,autoscaling group,auroscaling policy
--- Jenkins/demo_deploy/terraform_ec2/catalogue.sh
+++ Jenkins/demo_deploy/terraform_ec2/catalogue.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-yum install nginx -y
-yum start nginx -y
-yum enable nginx -y
-# APP_VERSION=$1
-# yum install python3 python3-pip -y
-# pip3 install --upgrade pip
-# pip3 install ansible
+APP_VERSION=$1
+yum install python3 python3-pip -y
+pip3 install --upgrade pip
+pip3 install ansible
+ansible-playbook -i inventory -e ansible_user=ec2-user --private-key=/home/ec2-user/Linux/Jenkins/terraform.pem Ping.yaml
 # cd /tmp
 #ansible-pull -U https://github.com/cryl91/branch.git main.yaml -e app_version=$APP_VERSION
 
