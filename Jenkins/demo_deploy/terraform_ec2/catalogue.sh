79e23a04f5aa1232035656f5e2604b3e6c270991
cyril <cryl91@gmail.com>
2025-04-09 22:08:24 +0530

    First

---
+++ Jenkins/demo_deploy/terraform_ec2/catalogue.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum install nginx -y
+yum start nginx -y
+yum enable nginx -y
+# APP_VERSION=$1
+# yum install python3 python3-pip -y
+# pip3 install --upgrade pip
+# pip3 install ansible
+# cd /tmp
+#ansible-pull -U https://github.com/cryl91/branch.git main.yaml -e app_version=$APP_VERSION
+
+##In the above github account keep the ansible code to install the application
