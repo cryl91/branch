3787ece196756be23e437a7cb322dbc9f3dd261c
cyril <cryl91@gmail.com>
2025-04-10 23:50:47 +0530

    First

---
+++ Terraform/createrole/4ec2.tf
@@ -0,0 +1,40 @@
+//Creating role
+
+
+resource "aws_iam_role" "ec2" {
+  name = "ec2_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+//Manily provide the policy arn like below to give premissions to role
+//managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"]
+  tags = {
+    Terraform = "true"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_ssm_access" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+
+
+
+# resource "aws_instance" "myinstance" {
+#    ami                     = var.ami_id
+#    instance_type           = var.instance_name == "mongodb" ? "t2.micro" : "t2.small"
+#     tags = var.tags
+#  } 
