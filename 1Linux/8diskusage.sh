d2e74b4ceae126b934128bf8677be68541226ac3
cyril <cryl91@gmail.com>
2025-06-26 03:07:12 +0530

    First

--- 1Linux/8diskusage.sh
+++ 1Linux/8diskusage.sh
@@ -7,7 +7,7 @@ message=""
 while IFS= read line
 do
 usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
-partition=$(echo $line | awk \ '{print $1}')
+partition=$(echo $line | awk '{print $1}')
 if [ $usage -gt $disk_usage_threshold ]
 then
 message+="High disk usage on $partition : $usage \n" 
